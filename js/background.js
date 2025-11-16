0c084b72a652bf140c695f5ad905d106010d37f9
Seth Gorelik <sethrgorelik@gmail.com>
2025-11-15 18:47:06 -0600

    changed background to colored waves, changed font to dosis, and updated instagram handle

--- js/background.js
+++ js/background.js
@@ -1,19 +1,34 @@
-function randomAngleNumber() {
-    return Math.floor(Math.random() * 360);
-}
+// background effect by Louis Hoebregts, source: 
+// https://codepen.io/Mamboleoo/pen/dypZQGo
 
-function formatAngle(num) {
-    return num + 'deg';
-}
+const paths = document.querySelectorAll('path');
+const defs = document.querySelector('defs');
+const xmlns = "http://www.w3.org/2000/svg";
 
-
-window.addEventListener('DOMContentLoaded', () => {
-    const root = document.documentElement;
-
-    const angle1Num = randomAngleNumber();
-    const angle2Num = (angle1Num + 90) % 360; // 90° apart
-
-    root.style.setProperty('--angle1', formatAngle(angle1Num));
-    root.style.setProperty('--angle2', formatAngle(angle2Num));
-
+paths.forEach((p, i) => {
+    const clone = p.cloneNode();
+    clone.setAttribute('stroke-dasharray', '');
+    const mask = document.createElementNS(xmlns, 'mask');
+    mask.setAttribute('id', `id-${i}`);
+    mask.appendChild(clone);
+    defs.appendChild(mask);
+    p.setAttribute('mask', `url(#id-${i})`);
+    gsap.set(clone, {
+        strokeDasharray: () => clone.getTotalLength(),
+        strokeDashoffset: () => clone.getTotalLength()
+    });
+    gsap.to(clone, {
+        duration: 10,
+        delay: i * 0.1,
+        repeat: -1,
+        strokeDashoffset: clone.getTotalLength() * 3,
+        ease: 'power1.inOut'
+    });
+    gsap.to(p, {
+        duration: 10,
+        repeat: -1,
+        strokeDashoffset: clone.getTotalLength() * 0.4,
+        ease: 'none'
+    });
 });
+document.querySelector('svg').style.opacity = 1;
