bd28024725ef20315ef4a53d5895cd8df91e23b0
Seth Gorelik <sethrgorelik@gmail.com>
2025-11-24 22:11:39 -0600

    beefed up portfolio

---
+++ etc/downsample_images.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+library(magick)
+
+imgs <- list.files(path = 'img/full', full.names = T)
+
+downsample.image <- function(f.in, sizes = c(400, 600)) {
+
+	# read image and get file name and extension
+	img <- image_read(f.in)
+	f.name <- tools::file_path_sans_ext(basename(f.in))
+	f.ext <- tools::file_ext(f.in)
+
+	# first convert input to png
+	if (f.ext != 'png') {
+		f.png <- gsub(f.ext, 'png', f.in)
+		image_write(img, path = f.png, format = 'png')
+		unlink(f.in)
+	}
+
+	# resize image proportionally to width sizes provided
+	for (size in sizes) {
+		img.scaled <- image_scale(img, paste0(size, 'x'))
+		dir.out <- paste0('img/p', size)
+		dir.create(dir.out, recursive = T, showWarnings = F)
+		f.out <- paste0(dir.out, '/', f.name, '_', size, '.png')
+		image_write(img.scaled, path = f.out, format = 'png')
+	}
+}
+
+catch <- lapply(imgs, downsample.image)
--- js/portfolio.js
+++ js/portfolio.js
@@ -3,16 +3,18 @@ jQuery("#gallery")
 		captions: false,
 		lastRow: "hide",
 		rowHeight: 180,
-		margins: 5
+		margins: 6
 	})
 	.on("jg.complete", function () {
 		window.lightGallery(
 			document.getElementById("gallery"), {
 				plugins: [lgZoom],
 				download: false,
+				mode: 'lg-fade',
+				height: '95%',
 				mobileSettings: {
 					controls: false,
-					showCloseIcon: false,
+					showCloseIcon: true,
 					download: false,
 					rotate: false
 				}
